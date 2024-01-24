e109e50738e3421efefc8ca8fef3e97e5f36bd54
Cavalchi <jpcavalchikar@hotmail.com>
2024-01-24 17:55:15 -0300

    .

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #Usar versão 3.8 do python ou inferior
+#pip install rasa
+#pip uninstall python-dateutil sqlalchemy
+#pip install python-dateutil==2.7.5 sqlalchemy==1.2.19
+#pip uninstall rasa pykwalify
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-chatbot = ChatBot('Meu ChatBot')
+chatbot = ChatBot('Meu ChatBot', storage_adapter='chatterbot.storage.SQLStorageAdapter')
 trainer = ChatterBotCorpusTrainer(chatbot)
 
 # Treinando o chatbot com o corpus em português
